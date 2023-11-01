60e68cd0ece2b03752d64aea004aaef77bdf5708
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-01 17:41:02 -0300

    Criado o model Acesso

--- src/main/java/com/mocad/ecommerce/model/Acesso.java
+++ src/main/java/com/mocad/ecommerce/model/Acesso.java
@@ -1,22 +1,29 @@
 package com.mocad.ecommerce.model;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table(name = "categoria_produto")
-@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto",
-        allocationSize = 1, initialValue = 1)
-public class CategoriaProduto implements Serializable {
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+
   private static final long serialVersionUID = 1L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
   private Long id;
 
-  @Column(name = "nome_desc", nullable = false)
-  private String nomeDesc;
+  @Column(name = "descricao", nullable = false)
+  private String descricao; /* Acesso ex: ROLE_ADMIN, ROLE_USUARIO */
+
+  @Override
+  public String getAuthority() {
+    return this.descricao;
+  }
 
   public Long getId() {
     return id;
@@ -26,19 +33,19 @@ public class CategoriaProduto implements Serializable {
     this.id = id;
   }
 
-  public String getNomeDesc() {
-    return nomeDesc;
+  public String getDescricao() {
+    return descricao;
   }
 
-  public void setNomeDesc(String nomeDesc) {
-    this.nomeDesc = nomeDesc;
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
   }
 
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
-    CategoriaProduto other = (CategoriaProduto) o;
+    Acesso other = (Acesso) o;
     if (id == null) {
       if (other.id != null) return false;
     } else if (!id.equals(other.id))
