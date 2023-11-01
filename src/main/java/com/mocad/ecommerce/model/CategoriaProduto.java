a55edb82d902b9072af0c7173f944a22b710cb5a
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-01 16:59:27 -0300

    Criado o model CategoriaProduto

---
+++ src/main/java/com/mocad/ecommerce/model/CategoriaProduto.java
@@ -0,0 +1,55 @@
+package com.mocad.ecommerce.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto",
+        allocationSize = 1)
+public class CategoriaProduto implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+  private Long id;
+
+  @Column(name = "nome_desc", nullable = false)
+  private String nomeDesc;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNomeDesc() {
+    return nomeDesc;
+  }
+
+  public void setNomeDesc(String nomeDesc) {
+    this.nomeDesc = nomeDesc;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    CategoriaProduto other = (CategoriaProduto) o;
+    if (id == null) {
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    return prime * result + ((id == null) ? 0 : id.hashCode());
+  }
+}
--- src/main/java/com/mocad/ecommerce/model/MarcaProduto.java
+++ src/main/java/com/mocad/ecommerce/model/MarcaProduto.java
@@ -2,17 +2,20 @@ package com.mocad.ecommerce.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "marca_produto")
 @SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
 public class MarcaProduto implements Serializable {
+
   private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
   private Long id;
 
+  @Column(name = "nome_desc", nullable = false)
   private String nomeDesc;
 
   public Long getId() {
@@ -30,4 +33,5 @@ public class MarcaProduto implements Serializable {
   public void setNomeDesc(String nomeDesc) {
     this.nomeDesc = nomeDesc;
   }
+
 }
