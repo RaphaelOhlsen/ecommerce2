8acf36cc6ed532f9451559fd7d10d6c6322f40ab
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-06 15:50:23 -0300

    Criado o model ContaReceber

--- src/main/java/com/mocad/ecommerce/model/Endereco.java
+++ src/main/java/com/mocad/ecommerce/model/Endereco.java
@@ -122,7 +122,7 @@ public class Endereco implements Serializable {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-   return  prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+   return  prime * result + ((id == null) ? 0 : id.hashCode());
   }
 
   @Override
