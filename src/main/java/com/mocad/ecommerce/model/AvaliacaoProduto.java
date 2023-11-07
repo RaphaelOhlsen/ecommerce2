14473f3daec026ef90c6395f5aec493d5756907e
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-07 17:29:05 -0300

    Criado o Model AvaliacaoProduto

---
+++ src/main/java/com/mocad/ecommerce/model/AvaliacaoProduto.java
@@ -0,0 +1,91 @@
+package com.mocad.ecommerce.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "avaliacao_produto")
+@SequenceGenerator(name = "seq_avaliacao_produto", sequenceName = "seq_avaliacao_produto", allocationSize = 1, initialValue = 1)
+public class AvaliacaoProduto implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+  private Long id;
+
+
+  private String descricao;
+
+  private Integer nota;
+
+  @ManyToOne(targetEntity = Pessoa.class)
+  @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(
+          value = ConstraintMode.CONSTRAINT, name= "pessoa_fk"))
+  private Pessoa pessoa;
+
+  @ManyToOne
+  @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(
+          value = ConstraintMode.CONSTRAINT, name= "produto_fk"))
+  private Produto produto;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Integer getNota() {
+    return nota;
+  }
+
+  public void setNota(Integer nota) {
+    this.nota = nota;
+  }
+
+  public Pessoa getPessoa() {
+    return pessoa;
+  }
+
+  public void setPessoa(Pessoa pessoa) {
+    this.pessoa = pessoa;
+  }
+
+  public Produto getProduto() {
+    return produto;
+  }
+
+  public void setProduto(Produto produto) {
+    this.produto = produto;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    return prime * result + ((id == null) ? 0 : id.hashCode());
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+    AvaliacaoProduto other = (AvaliacaoProduto) obj;
+    if (id == null) {
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+}
