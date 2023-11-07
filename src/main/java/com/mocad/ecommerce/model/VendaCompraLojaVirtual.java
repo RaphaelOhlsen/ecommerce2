da7f38bdedb853f30b1f90a6e22a55677c173273
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-07 14:50:46 -0300

    Criado o Model ItemVendaProduto

--- src/main/java/com/mocad/ecommerce/model/VendaCompraLojaVirtual.java
+++ src/main/java/com/mocad/ecommerce/model/VendaCompraLojaVirtual.java
@@ -60,4 +60,128 @@ public class VendaCompraLojaVirtual implements Serializable {
           value = ConstraintMode.CONSTRAINT, name = "forma_pagamento_fk"))
   private FormaPagamento formapagamento;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public BigDecimal getValorTotal() {
+    return valorTotal;
+  }
+
+  public void setValorTotal(BigDecimal valorTotal) {
+    this.valorTotal = valorTotal;
+  }
+
+  public BigDecimal getValorDesconto() {
+    return valorDesconto;
+  }
+
+  public void setValorDesconto(BigDecimal valorDesconto) {
+    this.valorDesconto = valorDesconto;
+  }
+
+  public BigDecimal getValorFrete() {
+    return valorFrete;
+  }
+
+  public void setValorFrete(BigDecimal valorFrete) {
+    this.valorFrete = valorFrete;
+  }
+
+  public int getDias_entrega() {
+    return dias_entrega;
+  }
+
+  public void setDias_entrega(int dias_entrega) {
+    this.dias_entrega = dias_entrega;
+  }
+
+  public Date getDataVenda() {
+    return dataVenda;
+  }
+
+  public void setDataVenda(Date dataVenda) {
+    this.dataVenda = dataVenda;
+  }
+
+  public Date getDataEntrega() {
+    return dataEntrega;
+  }
+
+  public void setDataEntrega(Date dataEntrega) {
+    this.dataEntrega = dataEntrega;
+  }
+
+  public Pessoa getPessoa() {
+    return pessoa;
+  }
+
+  public void setPessoa(Pessoa pessoa) {
+    this.pessoa = pessoa;
+  }
+
+  public Endereco getEnderecoEntrega() {
+    return enderecoEntrega;
+  }
+
+  public void setEnderecoEntrega(Endereco enderecoEntrega) {
+    this.enderecoEntrega = enderecoEntrega;
+  }
+
+  public Endereco getEnderecoCobranca() {
+    return enderecoCobranca;
+  }
+
+  public void setEnderecoCobranca(Endereco enderecoCobranca) {
+    this.enderecoCobranca = enderecoCobranca;
+  }
+
+  public NotaFiscalVenda getNotaFiscalVenda() {
+    return notaFiscalVenda;
+  }
+
+  public void setNotaFiscalVenda(NotaFiscalVenda notaFiscalVenda) {
+    this.notaFiscalVenda = notaFiscalVenda;
+  }
+
+  public CupDesc getCupDesc() {
+    return cupDesc;
+  }
+
+  public void setCupDesc(CupDesc cupDesc) {
+    this.cupDesc = cupDesc;
+  }
+
+  public FormaPagamento getFormapagamento() {
+    return formapagamento;
+  }
+
+  public void setFormapagamento(FormaPagamento formapagamento) {
+    this.formapagamento = formapagamento;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    return prime * result + ((id == null) ? 0 : id.hashCode());
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+    VendaCompraLojaVirtual other = (VendaCompraLojaVirtual) obj;
+    if (id == null) {
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
 }
