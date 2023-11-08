3ba0de773c856e86c360976e13aab35a1192fcb5
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-08 19:08:36 -0300

    Anotada a classe Applicatio do Spring Boot

--- src/main/java/com/mocad/ecommerce/EcommerceApplication.java
+++ src/main/java/com/mocad/ecommerce/EcommerceApplication.java
@@ -3,9 +3,15 @@ package com.mocad.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.mocad.ecommerce.model")
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.mocad.ecommerce.repository"})
+@EnableTransactionManagement
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
