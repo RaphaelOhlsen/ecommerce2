e9ab5193a4b081a5e35b34e4164e43e4d3e68ac1
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-16 10:15:15 -0300

    Criando a validacao do Token, liberacao do token

--- src/main/java/com/mocad/ecommerce/security/JWTTokenAutenticacaoService.java
+++ src/main/java/com/mocad/ecommerce/security/JWTTokenAutenticacaoService.java
@@ -1,10 +1,18 @@
 package com.mocad.ecommerce.security;
 
+import com.mocad.ecommerce.ApplicationContextLoad;
+import com.mocad.ecommerce.model.Usuario;
+import com.mocad.ecommerce.repository.UsuarioRepository;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
@@ -40,7 +48,59 @@ public class JWTTokenAutenticacaoService {
 
        response.addHeader(HEADER_STRING, token); /*Adiciona no cabeçalho http*/
 
+        liberacaoCors(response); /*Liberação de CORS*/
+
        /*Usado para ver no Insomnia para efeito de teste*/
        response.getWriter().write("{\"Authorization\": \""+token+"\"}"); /*Escreve token como resposta no corpo http*/
+    }
+
+    /* Retorna o usuario validado com token ou caso não seka válido retorna null */
+    public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        /*Pega o token enviado no cabeçalho http*/
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null) {
+            String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim(); /*Remove o prefixo do token*/
+
+            /*Faz a validação do token do usuário na requisicão*/
+            String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(tokenLimpo).getBody().getSubject();
+
+            if (user != null) {
+                Usuario usuario = ApplicationContextLoad.
+                        getApplicationContext().
+                        getBean(UsuarioRepository.class).findUserByLogin(user);
+                if (usuario != null) {
+                    return new UsernamePasswordAuthenticationToken(
+                            usuario.getLogin(),
+                            usuario.getSenha(),
+                            usuario.getAuthorities()
+                    ); /*Retorna as autorizações do usuário*/
+                }
+            }
+        }
+
+
+        liberacaoCors(response);
+        return null; /*Não autorizado*/
+
+    }
+
+    /* Fazendo a liberaćão contra erro de CORS no navegadpor */
+    private void liberacaoCors(HttpServletResponse response) {
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeaders("Access-Control-Requested-Headers") == null) {
+            response.addHeader("Access-Control-Requested-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
     }
 }
