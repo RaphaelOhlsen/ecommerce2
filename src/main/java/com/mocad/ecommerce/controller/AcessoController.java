cdd9012e8d1a5407bcc8a4ec58f27967d90a4c81
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-17 16:58:38 -0300

    Implementada excessões

--- src/main/java/com/mocad/ecommerce/controller/AcessoController.java
+++ src/main/java/com/mocad/ecommerce/controller/AcessoController.java
@@ -29,22 +29,31 @@ public class AcessoController {
   @Autowired
 	private AcessoRepository acessoRepository;
 
-  @ResponseBody
-  @PostMapping("**/salvarAcesso")
-  public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
-	
-	Acesso acessoSalvo = acessoService.save(acesso);
-	  
-	return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
-  }
+	@ResponseBody /*Poder dar um retorno da API*/
+	@PostMapping(value = "**/salvarAcesso") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) throws ExceptionEcommerce { /*Recebe o JSON e converte pra Objeto*/
+
+		if (acesso.getId() == null) {
+			List<Acesso> acessos = acessoRepository.buscarAcessoDesc(acesso.getDescricao().toUpperCase());
+
+			if (!acessos.isEmpty()) {
+				throw new ExceptionEcommerce("Já existe Acesso com a descrição: " + acesso.getDescricao());
+			}
+		}
+
+
+		Acesso acessoSalvo = acessoService.save(acesso);
+
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+	}
   
   @ResponseBody /*Poder dar um retorno da API*/
   @PostMapping(value = "**/deleteAcesso") /*Mapeando a url para receber JSON*/
 	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) { /*Recebe o JSON e converte pra Objeto*/
-		
+
 		acessoRepository.deleteById(acesso.getId());
 		
-		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
+		return new ResponseEntity<>("Acesso Removido",HttpStatus.OK);
 	}
   
   @ResponseBody
@@ -73,8 +82,8 @@ public class AcessoController {
   @GetMapping(value = "**/buscarPorDesc/{desc}") /*Mapeando a url para receber JSON*/
 	public ResponseEntity<List<Acesso>> buscarPorDesc(@PathVariable String desc) {
 		
-		List<Acesso> acesso = acessoRepository.buscarAcessoDesc(desc);
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc(desc.toUpperCase());
 		
-		return new ResponseEntity<List<Acesso>>(acesso, HttpStatus.OK);
+		return new ResponseEntity<List<Acesso>>(acessos, HttpStatus.OK);
 	}
 }
