90106c5212fa623e765a32a8b24a98c43b4f1a2f
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-21 10:43:39 -0300

    Implementada a interface AsyncConfigurer

--- src/main/java/com/mocad/ecommerce/EcommerceApplication.java
+++ src/main/java/com/mocad/ecommerce/EcommerceApplication.java
@@ -3,22 +3,44 @@ package com.mocad.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.concurrent.Executor;
+
 @SpringBootApplication
+@EnableAsync
 @EntityScan(basePackages = "com.mocad.ecommerce.model")
 @ComponentScan(basePackages = {"com.*"})
 @EnableJpaRepositories(basePackages = {"com.mocad.ecommerce.repository"})
 @EnableTransactionManagement
-public class EcommerceApplication {
+public class EcommerceApplication implements AsyncConfigurer {
 
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+	@Override
+	@Bean
+	public Executor getAsyncExecutor() {
+
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(20);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Asynchronous Thread");
+		executor.initialize();
+
+		return executor;
 	}
 
 }
