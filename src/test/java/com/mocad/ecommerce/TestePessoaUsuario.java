e65f9052bd80688ef5fd3cb822aa4ba346b6a147
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-21 10:33:28 -0300

    Adicionado a parte de endereco ao teste unitário

--- src/test/java/com/mocad/ecommerce/TestePessoaUsuario.java
+++ src/test/java/com/mocad/ecommerce/TestePessoaUsuario.java
@@ -1,6 +1,8 @@
 package com.mocad.ecommerce;
 
 import com.mocad.ecommerce.controller.PessoaController;
+import com.mocad.ecommerce.enums.TipoEndereco;
+import com.mocad.ecommerce.model.Endereco;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,6 +37,46 @@ public class TestePessoaUsuario extends TestCase{
         pessoaJuridica.setRazaoSocial("Armazem Raphael LTDA");
         pessoaJuridica.setTipoPessoa("pessoaJuridica");
 
-        pessoaController.salvarPJ(pessoaJuridica);
+
+        Endereco endereco1 = new Endereco();
+        endereco1.setBairro("Jd Dias");
+        endereco1.setCep("556556565");
+        endereco1.setComplemento("Casa cinza");
+        endereco1.setEmpresa(pessoaJuridica);
+        endereco1.setNumero("389");
+        endereco1.setPessoa(pessoaJuridica);
+        endereco1.setRuaLogra("Av. são joao sexto");
+        endereco1.setTipoEndereco(TipoEndereco.COBRANCA);
+        endereco1.setUf("PR");
+        endereco1.setCidade("Curitiba");
+
+
+        Endereco endereco2 = new Endereco();
+        endereco2.setBairro("Jd Maracana");
+        endereco2.setCep("7878778");
+        endereco2.setComplemento("Andar 4");
+        endereco2.setEmpresa(pessoaJuridica);
+        endereco2.setNumero("555");
+        endereco2.setPessoa(pessoaJuridica);
+        endereco2.setRuaLogra("Av. maringá");
+        endereco2.setTipoEndereco(TipoEndereco.ENTREGA);
+        endereco2.setUf("PR");
+        endereco2.setCidade("Curitiba");
+
+        pessoaJuridica.getEnderecos().add(endereco2);
+        pessoaJuridica.getEnderecos().add(endereco1);
+
+        pessoaJuridica = pessoaController.salvarPJ(pessoaJuridica).getBody();
+
+        assertEquals(true, pessoaJuridica.getId() > 0 );
+
+        for (Endereco endereco : pessoaJuridica.getEnderecos()) {
+            assertEquals(true, endereco.getId() > 0);
+        }
+
+        assertEquals(2, pessoaJuridica.getEnderecos().size());
+
+
+
     }
 }
