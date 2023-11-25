86febd49853f1d280c901e07c83eafe9527cec5f
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-11-25 09:48:23 -0300

    Criado DTOs para consulta de CNPJ

--- src/main/java/com/mocad/ecommerce/controller/PessoaController.java
+++ src/main/java/com/mocad/ecommerce/controller/PessoaController.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
