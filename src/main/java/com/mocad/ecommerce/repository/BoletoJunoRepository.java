990afa77b485db8078fd50a295d6bd1f994b9359
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-12-14 11:49:08 -0300

    Criado End-Point de recebimento de Pix e bolte

--- src/main/java/com/mocad/ecommerce/repository/BoletoJunoRepository.java
+++ src/main/java/com/mocad/ecommerce/repository/BoletoJunoRepository.java
@@ -2,9 +2,26 @@ package com.mocad.ecommerce.repository;
 
 import com.mocad.ecommerce.model.BoletoJuno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BoletoJunoRepository extends JpaRepository<BoletoJuno, Long> {
+
+  @Query("select b from BoletoJuno b where b.code = ?1")
+  BoletoJuno findByCode (String code);
+
+  @Modifying(flushAutomatically = true)
+  @Query(nativeQuery = true, value = "update boleto_juno set quitado = true where code = ?1")
+  void quitarBoleto(String code);
+
+
+  @Transactional
+  @Modifying(flushAutomatically = true)
+  @Query(nativeQuery = true, value = "update boleto_juno set quitado = true where id = ?1")
+  void quitarBoletoById(Long id);
 
 }
--- src/main/java/com/mocad/ecommerce/security/WebConfigSecurity.java
+++ src/main/java/com/mocad/ecommerce/security/WebConfigSecurity.java
@@ -34,6 +34,8 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		.disable().authorizeRequests().antMatchers("/").permitAll()
 		.antMatchers("/index").permitAll()
+		.antMatchers(HttpMethod.POST, "/requisicaojunoboleto/**", "/notificacaoapiv2").permitAll()
+		.antMatchers(HttpMethod.GET, "/requisicaojunoboleto/**", "/notificacaoapiv2").permitAll()
 		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 		
 		/* redireciona ou da um retorno para index quando desloga*/
@@ -57,15 +59,16 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 		
 	}
-	
-	
 
-	/*Ignora alguas URL livre de autenticação*/
+
+
+	/* Ignora algumas URL livre de autenticação */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		//web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
-		//.antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
-		/*Ingnorando URL no momento para nao autenticar*/
+		web.ignoring().
+				antMatchers(HttpMethod.GET, "/requisicaojunoboleto/**", "/notificacaoapiv2")
+				.antMatchers(HttpMethod.POST,"/requisicaojunoboleto/**", "/notificacaoapiv2");
+		/* Ingnorando URL no momento para nao autenticar */
 	}
 
 }
